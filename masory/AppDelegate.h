94e73ab74f88bb675d3235d12efd88fb43913046
admin <1395258209@qq.com>
2017-02-04 21:49:17 +0800

    创建一个项目，并导入Masory

---
+++ masory/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  masory
+//
+//  Created by admin on 17/2/4.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
